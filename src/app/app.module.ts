b25bf605e8881c88775cb89ac725c7e3ceb4037a
Patrick Almeida <patricksalmeida@gmail.com>
2022-10-10 15:35:48 -0300

    feat: add login and dashboard pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+
+export const routes: Routes = [
+  { path: '', loadChildren: () => import('./features/public/public.module').then(m => m.PublicModule) },
+  { path: 'app', loadChildren: () => import('./features/private/private.module').then(m => m.PrivateModule) },
+]
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
